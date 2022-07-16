ee3656d94e2c4d2a86ec6907c5afdf4a90fbcb82
Serge Bouts <45689002+irizen@users.noreply.github.com>
2022-07-16 16:31:34 -0400

    Initial Commit

---
+++ WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by Serge Bouts on 2022-07-16.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
